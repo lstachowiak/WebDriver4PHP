6b9e2a47755444f3f21ac7455e812d4c36d9ac15
lstachowiak <lstachowiak@lstachowiak.pl>
2011-12-14 21:14:48 +0100

    adding w4p libraries in boostrap-core

--- src/boostrap-core.php
+++ src/boostrap-core.php
@@ -1,3 +1,7 @@
 <?php
+echo getcwd();
+set_include_path('./library/' . PATH_SEPARATOR 
+				. get_include_path());
 
-
+require_once ("webdriver4php/src/WebDriver.php");
+require_once ("webdriver4php/src/LocatorStrategy.php");
